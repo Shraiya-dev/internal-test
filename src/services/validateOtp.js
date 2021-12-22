695aa0fe709b24d9546cb0c76c650a5b5dd2f45e
puneetsingh99 <99singhpuneet99@gmail.com>
2021-12-23 01:51:36 +0530

    chore: remove console.log

--- src/services/validateOtp.js
+++ src/services/validateOtp.js
@@ -7,7 +7,6 @@ export const validateOtp = async (phoneNumber, otp) => {
   const payload = { ult, ut, phoneNumberRaw: phoneNumber, otp };
   try {
     const res = await axios.post(VALIDATE_OTP_API, { userData: payload });
-    console.log(res);
     return res;
   } catch (error) {
     return { success: false, error: error.message };
